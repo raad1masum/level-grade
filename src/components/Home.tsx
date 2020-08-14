e37b089a77089e2bb7fc96af7ed3c5779f7da54a
raad1masum <piraadwan@gmail.com>
2020-08-14 15:20:37 -0700

    get word count

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,31 +2,36 @@ import React, { useState } from "react";
 
 // create an interface for your state
 interface info {
-  text: String;
+    text: String;
 }
 
 const Home = () => {
-  // create a useState hook for a state variable
-  const [infoState, setInfoState] = useState<info>({
-    text: "",
-  });
+    // create a useState hook for a state variable
+    const [infoState, setInfoState] = useState<info>({
+        text: "",
+    });
 
-  const onchange = (e: any) => {
-    const newValue = e.currentTarget.value;
-    setInfoState({ text: newValue });
-  };
+    const onchange = (e: any) => {
+        const newValue = e.currentTarget.value;
+        setInfoState({ text: newValue });
+    };
 
-  const algo = (s: String) => {
-    return s;
-  };
+    const algo = (str: String) => {
+        if (str.length == 0) {
+            return 0;
+        } else {
+            const wordCount: number = str.trim().split(/\s+/).length
+            return wordCount;
+        }
+    };
 
-  return (
-    <>
-      <h1>Level Grade</h1>
-      <textarea name="name" onChange={onchange}></textarea>
-      <p>{algo(infoState.text)}</p>
-    </>
-  );
+    return (
+        <>
+            <h1>Level Grade</h1>
+            <textarea name="name" onChange={onchange}></textarea>
+            <p>{algo(infoState.text)}</p>
+        </>
+    );
 };
 
 export default Home;
