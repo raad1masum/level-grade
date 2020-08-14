594d203209dfadc841b13edfe8b9ebeeae7a27ad
raad1masum <piraadwan@gmail.com>
2020-08-14 13:39:28 -0700

    create text area and return text in text area

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+// create an interface for your state
+interface info {
+  text: String;
+}
 
 const Home = () => {
+  // create a useState hook for a state variable
+  const [infoState, setInfoState] = useState<info>({
+    text: "",
+  });
+
+  const onchange = (e: any) => {
+    const newValue = e.currentTarget.value;
+    setInfoState({ text: newValue });
+  };
+
+  const algo = (s: String) => {
+    return s;
+  };
+
   return (
     <>
       <h1>Level Grade</h1>
-      <form>
-        <label>
-          <input type="text" name="name" />
-        </label>
-      </form>
+      <textarea name="name" onChange={onchange}></textarea>
+      <p>{algo(infoState.text)}</p>
     </>
   );
 };
