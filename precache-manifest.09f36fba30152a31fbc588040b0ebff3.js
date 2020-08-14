bb3fe813e9d89ae7761bab76771a1bafee305747
Raadwan Masum <piraadwan@gmail.com>
2020-08-14 16:28:21 -0700

    Updates

---
+++ precache-manifest.09f36fba30152a31fbc588040b0ebff3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299f5c16bb5b2b3e5a967a4cad5c1821",
+    "url": "/level-grade/index.html"
+  },
+  {
+    "revision": "b3a50b6c8ce0b5398877",
+    "url": "/level-grade/static/js/2.01ea4d93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/level-grade/static/js/2.01ea4d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58753e5e26dbfd0c8ad2",
+    "url": "/level-grade/static/js/main.e61a8b72.chunk.js"
+  },
+  {
+    "revision": "67d52489eaffde754fd8",
+    "url": "/level-grade/static/js/runtime-main.e1aae50b.js"
+  }
+]);
