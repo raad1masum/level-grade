04e98729a81d60c11fdd6fc5c2dd84deded068cd
raad1masum <piraadwan@gmail.com>
2020-08-14 13:23:58 -0700

    add basic stuffs

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Level Grade</h1>
+      <form>
+        <label>
+          <input type="text" name="name" />
+        </label>
+      </form>
+    </>
+  );
+};
+
+export default Home;
