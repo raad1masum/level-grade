394169437ebff47976d89fc17c74aa4bee050586
raad1masum <piraadwan@gmail.com>
2020-08-14 15:22:12 -0700

    clean up code a bit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,28 +2,28 @@ import React, { useState } from "react";
 
 // create an interface for your state
 interface info {
-    text: String;
+    text: String
 }
 
 const Home = () => {
     // create a useState hook for a state variable
     const [infoState, setInfoState] = useState<info>({
         text: "",
-    });
+    })
 
     const onchange = (e: any) => {
-        const newValue = e.currentTarget.value;
-        setInfoState({ text: newValue });
-    };
+        const newValue = e.currentTarget.value
+        setInfoState({ text: newValue })
+    }
 
     const algo = (str: String) => {
+        const wordCount: number = str.trim().split(/\s+/).length
         if (str.length == 0) {
-            return 0;
+            return 0
         } else {
-            const wordCount: number = str.trim().split(/\s+/).length
-            return wordCount;
+            return wordCount
         }
-    };
+    }
 
     return (
         <>
@@ -31,7 +31,7 @@ const Home = () => {
             <textarea name="name" onChange={onchange}></textarea>
             <p>{algo(infoState.text)}</p>
         </>
-    );
-};
+    )
+}
 
 export default Home;
